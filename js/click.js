c41e3dae78d5607c2efdec7be1766f1c77c08f11
E2S2-NUS-SJTU <E2S2officialacc@gmail.com>
2019-02-23 17:23:54 +0800

    finished

--- js/click.js
+++ js/click.js
@@ -1,6 +1,10 @@
 var hide1 = 0;
 var hide2 = 0;
 var hide3 = 0;
+var hide4 = 0;
+var hide5 = 0;
+var hide6 = 0;
+var hide7 = 0;
 
 $(document).ready(function() {
     $("#btn1").click(function() {
@@ -40,6 +44,62 @@ $(document).ready(function() {
         else {
             $("#btn3").text(">>");
             hide3 = 0;
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#btn4").click(function() {
+        $("#Gasification").slideToggle("slow");
+        if (hide4 == 0) {
+            $("#btn4").text("<<");
+            hide4 = 1;
+        } 
+        else {
+            $("#btn4").text(">>");
+            hide4 = 0;
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#btn5").click(function() {
+        $("#DSS").slideToggle("slow");
+        if (hide5 == 0) {
+            $("#btn5").text("<<");
+            hide5 = 1;
+        } 
+        else {
+            $("#btn5").text(">>");
+            hide5 = 0;
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#btn6").click(function() {
+        $("#LCA").slideToggle("slow");
+        if (hide6 == 0) {
+            $("#btn6").text("<<");
+            hide6 = 1;
+        } 
+        else {
+            $("#btn6").text(">>");
+            hide6 = 0;
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#btn7").click(function() {
+        $("#UMM").slideToggle("slow");
+        if (hide7 == 0) {
+            $("#btn7").text("<<");
+            hide7 = 1;
+        } 
+        else {
+            $("#btn7").text(">>");
+            hide7 = 0;
         }
     });
 });
